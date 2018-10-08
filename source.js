2e29d76b18362aedf0977e02832092fbe0d5e5fd
nick <nickhidalgo93@gmail.com>
2018-10-08 16:13:21 -0400

    change two buttons to one

--- source.js
+++ source.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-    $("#hide").click(function(){
-        $("p").hide()
-    });
-    $("#show").click(function(){
-        $("p").show();
+    $("button").click(function(){
+        $("p").toggle();
     });
 });
