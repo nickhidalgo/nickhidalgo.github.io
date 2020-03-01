7106c73e4f67b3fff0e33aa1c21a44cbc30fe9ec
nick <nickhidalgo93@gmail.com>
2020-02-29 20:43:53 -0500

    change desgin

--- source.js
+++ source.js
@@ -1,34 +1,5 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("p").toggle();
-    });
-});
-$(document).ready(function () {
-    $('div.hidden').fadeIn(1000).removeClass('hidden');
-});
-$(document).ready(() => {
-	$('.selected-element').addClass('animate');
-});
 
-
-function myFunction() {
-    var element = document.header;
-    element.classList.toggle("dark-mode");
+function myFunction(){
+    document.footer.style.backgroundColor="black";  
  }
 
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("nav");
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else{
-        navbar.classList.remove("sticky");
-    }
-}
-
-//============================================================================//
