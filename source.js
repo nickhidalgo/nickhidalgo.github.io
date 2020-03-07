b7109f7244d14db8eae57765f3cf036844bf2ad5
nick <nickhidalgo93@gmail.com>
2020-03-06 23:15:11 -0500

    redisgn portfolio

--- source.js
+++ source.js
@@ -2,4 +2,26 @@
 function myFunction(){
     document.footer.style.backgroundColor="black";  
  }
+ // When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar navbar-fixed-top").style.top = "0";
+  } else {
+    document.getElementById("navbar navbar-fixed-top").style.top = "-50px";
+  }
+}
+// scroll functions
+$(window).scroll(function(e) {
+
+    // add/remove class to navbar when scrolling to hide/show
+    var scroll = $(window).scrollTop();
+    if (scroll >= 150) {
+        $('.navbar').addClass("navbar-hide");
+    } else {
+        $('.navbar').removeClass("navbar-hide");
+    }
+
+});
 
